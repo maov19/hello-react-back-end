667d1d29a161e29fa65edadbf7d57972aad5ee6a
mossa <martin.ossa.8@gmail.com>
2023-07-11 14:00:28 +0200

    Setup rails and models

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,7 @@
+class MessagesController < ApplicationController
+    def random_greeting
+      message = Message.order("RANDOM()").first
+      render json: { greeting: message&.content }
+    end
+end
+  
