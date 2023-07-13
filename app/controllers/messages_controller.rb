1c0261a70b1ecff4068ef3550eb502675df9a4bf
mossa <martin.ossa.8@gmail.com>
2023-07-13 09:50:41 +0200

    Fix cors issue

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,13 @@
 class MessagesController < ApplicationController
+
+  def set_cors_headers
+    response.set_header('Access-Control-Allow-Origin', '*')
+    response.set_header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+    response.set_header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, Token')
+  end
+  
+  before_action :set_cors_headers
+
   def random_greeting
     message = Message.order('RANDOM()').first
     render json: { greeting: message&.content }
