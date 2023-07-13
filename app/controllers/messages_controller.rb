37e774a3a9e8ba993ab55f8ec62ad6b2d78bf438
mossa <martin.ossa.8@gmail.com>
2023-07-13 09:53:31 +0200

    Fix styling issues

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,10 @@
 class MessagesController < ApplicationController
-
   def set_cors_headers
     response.set_header('Access-Control-Allow-Origin', '*')
     response.set_header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
     response.set_header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, Token')
   end
-  
+
   before_action :set_cors_headers
 
   def random_greeting
